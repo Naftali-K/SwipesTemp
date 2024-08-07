6b2bd1cf5156d9c4ec11f1037482893153adf1c2
nihonto <webhelp@ukr.net>
2024-08-07 10:11:28 +0300

    Horizontal Swipes class updated. Tested on RecyclerView  +  ViewPager

---
+++ app/src/main/java/com/nk/swipestemp/adapters/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.nk.swipestemp.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.nk.swipestemp.fragments.FirstFragment;
+import com.nk.swipestemp.fragments.SecondFragment;
+import com.nk.swipestemp.fragments.ThirdFragment;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            case 2:
+                return new ThirdFragment();
+            default:
+                return new FirstFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
